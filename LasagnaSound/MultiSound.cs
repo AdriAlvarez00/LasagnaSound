766b6d47105b067ebb4071885ffa8c0630ac7b70
AdriAlvarez00 <adralv01@ucm.es>
2021-11-27 23:34:44 +0100

    Plugin test

---
+++ LasagnaSound/MultiSound.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+namespace LasagnaSound
+{
+    public class MultiSound : MonoBehaviour
+    {
+        [SerializeField] private AudioClip[] clips;
+
+        void Start()
+        {
+            Debug.Log("Loaded " + clips.Length + " clips");
+            Debug.Log("oleole autocopy");
+        }
+    }
+}
